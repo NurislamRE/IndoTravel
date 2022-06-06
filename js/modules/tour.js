d16f381fa0555bd85dfbb36c99d777a336800372
Нурислам <nurislam.raushanbek@gmail.com>
2022-06-06 09:13:47 +0600

    update fix

--- js/modules/tour.js
+++ js/modules/tour.js
@@ -1,4 +1,4 @@
-const result = await fetch('https://bfs01.getcourse.ru/public/files/251231/195/cbe21783a515f15b9fc7c49f727da123.json?e=1654235999&s=37FM6DeqjanGVvMUZPTuKw');
+const result = await fetch('https://bfs01.getcourse.ru/public/files/251231/195/cbe21783a515f15b9fc7c49f727da123.json?e=1654495199&s=u0TsolC5hPzC2rR-mNNGIw');
 const data = await result.json();
 
 const tourForm = document.querySelector('.tour__form');
